11d4105d4406cf4c83ea6863674b55d61a02004c
조혜라 <s2037@e-mirim.hs.kr>
2022-08-09 18:21:07 +0900

    feat(argument)

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "world"
+	}
+	fmt.Fprintf(w, "Hello %s!", name)
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World!"))
 	})
+
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("test !!!"))
+	})
+
+	http.HandleFunc("/bar", barHandler)
 
 	http.ListenAndServe(":5000", nil)
 }
