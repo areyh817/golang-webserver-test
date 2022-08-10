fc7da7edfeef0598a73853405490b5fcd8662209
조혜라 <s2037@e-mirim.hs.kr>
2022-08-10 16:20:59 +0900

    test

--- main.go
+++ main.go
@@ -1,28 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func barHandler(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	if name == "" {
-		name = "world"
-	}
-	fmt.Fprintf(w, "Hello %s!", name)
-}
+const (
+	QueryRegister = `
+	insert into users(id, password)
+	values($1, $2)`
+)
+
+var DB *sql.DB
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+
+	r := gin.Default()
+	r.GET("/ping", pong)
+	r.POST("/orangina", orangina)
+	r.Run(":8081") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func pong(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "pong",
 	})
+}
 
-	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("test !!!"))
+func orangina(ctx *gin.Context) {
+	characterName := ctx.PostForm("character")
+	age := ctx.PostForm("age")
+
+	DB.ExecContext()
+	rows, err := DB.QueryContext(ctx, QueryRegister, id, password)
+	if err != nil{
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error()
+		})
+		return
+	}
+	
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "success",
 	})
-
-	http.HandleFunc("/bar", barHandler)
-
-	http.ListenAndServe(":5000", nil)
 }
